dbf7f2218c38535be1a98794572ecdb89f2dfee0
itzsourabh <85216659+itzsourabhkumar@users.noreply.github.com>
2023-09-23 21:57:30 +0530

    add code
    ecom MERN APP

---
+++ config/db.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import colors from "colors";
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(
+      `Connect to Mongodb database ${conn.connection.host}`.bgMagenta.white
+    );
+  } catch (error) {
+    console.log(`Error in Mongodb ${error}`.bgRed.white);
+  }
+};
+
+export default connectDB;
---
+++ routes/productRoute.js
@@ -0,0 +1,85 @@
+import express from "express";
+import { isAdmin, requireSignIn } from "./../middlewares/authMiddleware.js";
+import {
+  braintreePaymentController,
+  braintreeTokenController,
+  createProductController,
+  deleteProductController,
+  getProductController,
+  getSingleProductController,
+  productCategoryController,
+  productCountController,
+  productFilterController,
+  productListController,
+  productPhotoController,
+  reletedProductController,
+  searchProductController,
+  updateProductController,
+} from "./../controllers/productController.js";
+import formidable from "express-formidable";
+
+const router = express.Router();
+
+//routes
+//create product
+router.post(
+  "/create-product",
+  requireSignIn,
+  isAdmin,
+  formidable(),
+  createProductController
+);
+
+//get products
+router.get("/get-product", getProductController);
+
+//single product
+router.get("/get-product/:slug", getSingleProductController);
+
+//get photo
+router.get("/product-photo/:pid", productPhotoController);
+
+//delete product
+router.delete(
+  "/delete-product/:pid",
+  requireSignIn,
+  isAdmin,
+  formidable(),
+  deleteProductController
+);
+
+//update product
+router.put(
+  "/update-product/:pid",
+  requireSignIn,
+  isAdmin,
+  formidable(),
+  updateProductController
+);
+
+//filter product
+router.post("/product-filters", productFilterController);
+
+//product count
+router.get("/product-count", productCountController);
+
+//product per page
+router.get("/product-list/:page", productListController);
+
+//search product
+router.get("/search/:keyword", searchProductController);
+
+//similar product
+router.get("/related-product/:pid/:cid", reletedProductController);
+
+//category wise product
+router.get("/product-category/:slug", productCategoryController);
+
+//payments routes
+//token
+router.get("/braintree/token", braintreeTokenController);
+
+//payment
+router.post("/braintree/payment", requireSignIn, braintreePaymentController);
+
+export default router;
